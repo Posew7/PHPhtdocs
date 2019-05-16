089a4b0b1c5b3f1e24246402ad02c17b7dfa29fc
Posew7 <posew7@gmail.com>
2019-05-17 02:50:25 +0300

    array

---
+++ array.php
@@ -0,0 +1,23 @@
+<?php
+
+$ad = "posew7";
+$sayi = 7;
+
+$dizi = array($ad,$sayi,8.8,"test");
+
+print_r($dizi);
+
+print $dizi[0];
+
+$kimlik = array(
+    "ad" => "yunus",
+    "soyad" => "üstün",
+    "meslek" => "öğrenci",
+    "yas" => "23"
+);
+
+print_r($kimlik);
+
+print $kimlik["ad"];
+
+?>
--- ternary.php
+++ ternary.php
@@ -3,3 +3,5 @@
 $sayi = 7;
 
 print $sayi==7 ? "sayı 7ye eşit" : "sayı 7ye eşit değil";
+
+?>
