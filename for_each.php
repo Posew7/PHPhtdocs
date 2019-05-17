db511bcde14b184b197f349bfe7b44192bef8766
Posew7 <posew7@gmail.com>
2019-05-17 03:22:38 +0300

    for

---
+++ for_each.php
@@ -0,0 +1,20 @@
+<?php
+
+$dizi = [1,2,3,4,5,6,7,8,9];
+
+foreach ($dizi as $i){
+    print $i . "<br>";
+}
+
+$kimlik = array(
+    "ad" => "yunus",
+    "soyad" => "üstün",
+    "meslek" => "öğrenci",
+    "yas" => "23"
+);
+
+foreach ($kimlik as $key => $arg){
+    print $key . " = " . $arg . "<br>";
+}
+
+?>
