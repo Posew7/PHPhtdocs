2c068be52e21a5853dcda3d164043485b13f3ccb
Posew7 <posew7@gmail.com>
2019-05-18 18:38:48 +0300

    array_func

---
+++ array_func2.php
@@ -0,0 +1,33 @@
+<?php
+
+$arr = [1, 2, 3, 4, 5];
+
+$arr = array_map(function ($val) {
+    return $val * 2;
+}, $arr);
+
+//print_r($arr);
+
+$arr = ["yunus", "", "üstün", " ", "7", ""];
+
+$arr = array_filter($arr);
+
+//print_r($arr);
+
+$arr1 = [1, 2, 3];
+$arr2 = [4, 5, 6];
+$arr3 = array_merge($arr1, $arr2);
+
+//print_r($arr3);
+
+$arr = [
+    "ad" => "yunus",
+    "soyad" => "üstün",
+    "yaş" => 23
+];
+
+//print_r(array_rand($arr,2));
+
+$arr = array_reverse($arr);
+//print_r($arr);
+$arr = array_reverse($arr);
