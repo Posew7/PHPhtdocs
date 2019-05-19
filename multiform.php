a38a0f4c2f668c41698bcda9928428468f81cc4b
Posew7 <posew7@gmail.com>
2019-05-19 08:22:00 +0300

    get post

---
+++ multiform.php
@@ -0,0 +1,27 @@
+<h1>GİRİŞ YAP</h1>
+<form action="" method="post">
+    Kullanıcı Adı: <br>
+    <input type="text" name="kullanici">
+    <hr>
+    Şifre: <br>
+    <input type="password" name="sifre">
+    <hr>
+    <button type="submit">GİRİŞ</button>
+</form>
+
+<br><br><br>
+
+<h1>KAYIT OL</h1>
+<form action="" method="post">
+    Kullanıcı Adı: <br>
+    <input type="text" name="kullanici">
+    <hr>
+    Şifre: <br>
+    <input type="password" name="sifre">
+    <hr>
+    E-Posta: <br>
+    <input type="text" name="eposta">
+    <hr>
+    <button type="submit">KAYIT OL</button>
+</form>
+
